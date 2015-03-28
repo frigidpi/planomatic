f6c99168fbe16edfbf331ff4c4b80fa0df93cb32
qedpi <junruihu@gmail.com>
2015-03-28 01:41:33 -0400

    updated scheduler

--- src/autoplan/Scheduler.java
+++ src/autoplan/Scheduler.java
@@ -4,6 +4,17 @@ import java.util.List;
 
 public class Scheduler {
 
+	private List< Task > tasksOrdered;
+	private List< Event > events;
+	private List< Gap > gaps;
+
+	public addEvent( Event ev ){
+		events.add( ev );
+		Collections.sort( events, (Event ev1, Event ev2) -> (ev1.getStartDate()).compareTo(ev2.getStartDate()) );
+	}
+	
+	
+	
 	public List<Task> schedule() {
 		return null;
 	}
