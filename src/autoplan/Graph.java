24257e9fabf4981f4f9393f65ca0840a21cfc751
Duncan Cai <dcai5@uwo.ca>
2015-03-28 00:44:52 -0400

    Added graph class

---
+++ src/autoplan/Graph.java
@@ -0,0 +1,31 @@
+package autoplan;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Graph<T> {
+
+	private HashMap<T, List<T>> adj;
+	
+	public Graph() {
+		adj = new HashMap<>();
+	}
+	
+	public void addNode(T node) {
+		adj.put(node, new LinkedList<T>());
+	}
+	
+	public void addEdge(T u, T v) {
+		if(!adj.containsKey(u))
+			throw new RuntimeException("Graph does not contain " + u);
+		adj.get(u).add(v);
+	}
+	
+	public List<T> neighbours(T node) {
+		if(!adj.containsKey(node))
+			throw new RuntimeException("Graph does not contain " + node);
+		return adj.get(node);
+	}
+	
+}
---
+++ src/autoplan/Task.java
@@ -0,0 +1,5 @@
+package autoplan;
+
+public class Task {
+	
+}
